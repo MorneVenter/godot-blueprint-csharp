ac0b50d43982ff455b2a46c78732ef493f2804cf
Morne Venter <morne@Mornes-MacBook-Pro.local>
2024-03-03 12:44:46 +0200

    fix scene holder logic for errors

--- Assets/Scenes/CustomNodes/SceneHolder/Scene2D.cs
+++ Assets/Scenes/CustomNodes/SceneHolder/Scene2D.cs
@@ -2,18 +2,18 @@ using Godot;
 
 namespace Project.Assets.Scenes.CustomNodes.SceneHolder
 {
-	[Tool]
-	public partial class Scene2D : SubViewport
-	{
-		private const string BAD_PARENT_ERROR =
-			"This nodes needs to be a child of a SceneHolder node.";
+    [Tool]
+    public partial class Scene2D : SubViewport
+    {
+        private const string BAD_PARENT_ERROR =
+            "This nodes needs to be a child of a SceneHolder node.";
 
-		public override string[] _GetConfigurationWarnings()
-		{
-			var parent = GetParent();
-			return parent is not SceneHolder
-				? (new string[] { BAD_PARENT_ERROR })
-				: System.Array.Empty<string>();
-		}
-	}
+        public override string[] _GetConfigurationWarnings()
+        {
+            var parent = GetParent();
+            return parent is not SceneHolder
+                ? (new string[] { BAD_PARENT_ERROR })
+                : System.Array.Empty<string>();
+        }
+    }
 }
--- Assets/Scenes/CustomNodes/SceneHolder/SceneHolder.cs
+++ Assets/Scenes/CustomNodes/SceneHolder/SceneHolder.cs
@@ -2,39 +2,39 @@ using Godot;
 
 namespace Project.Assets.Scenes.CustomNodes.SceneHolder
 {
-	[Tool]
-	public partial class SceneHolder : SubViewportContainer
-	{
-		private const string ORDER_ERROR =
-			"A Scene3D node should not be placed lower in the node tree than a Scene2D.";
-		private const string NOT_FOUND_ERROR =
-			"This node doesn't have a Scene2D or Scene3D as a child. Add the required nodes or things will get weird.";
+    [Tool]
+    public partial class SceneHolder : SubViewportContainer
+    {
+        private const string ORDER_ERROR =
+            "A Scene3D node should not be placed lower in the node tree than a Scene2D.";
+        private const string NOT_FOUND_ERROR =
+            "This node doesn't have a Scene2D or Scene3D as a child. Add the required nodes or things will get weird.";
 
-		public override string[] _GetConfigurationWarnings()
-		{
-			var children = GetChildren();
-			var hasScene3D = false;
-			var hasScene2D = false;
+        public override string[] _GetConfigurationWarnings()
+        {
+            var children = GetChildren();
+            var hasScene3D = false;
+            var hasScene2D = false;
 
-			foreach (var child in children)
-			{
-				if (child is Scene3D)
-				{
-					hasScene3D = true;
-					if (hasScene2D)
-					{
-						return new string[] { ORDER_ERROR };
-					}
-				}
-				else if (child is Scene2D)
-				{
-					hasScene2D = true;
-				}
-			}
+            foreach (var child in children)
+            {
+                if (child is Scene3D)
+                {
+                    hasScene3D = true;
+                    if (hasScene2D)
+                    {
+                        return new string[] { ORDER_ERROR };
+                    }
+                }
+                else if (child is Scene2D)
+                {
+                    hasScene2D = true;
+                }
+            }
 
-			return !hasScene2D && hasScene3D
-				? (new string[] { NOT_FOUND_ERROR })
-				: System.Array.Empty<string>();
-		}
-	}
+            return hasScene2D || hasScene3D
+                ? System.Array.Empty<string>()
+                : (new string[] { NOT_FOUND_ERROR });
+        }
+    }
 }
